5111e55c40f37b81eca42d132c7f3377573e1b5e
damiankam <demon.kaminsky@gmail.com>
2016-05-30 22:13:45 +0300

    add hello.HelloController

---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,17 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by dima on 5/30/16.
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
